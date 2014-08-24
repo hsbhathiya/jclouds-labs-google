1a1a8be884799228b2b550ad7c07ef70240a38da
hsbhathiya <hsbathiya@gmail.com>
2014-08-24 11:17:32 +0530

    JCLOUDS-458:Changes in the package structure
    Package name change

--- google-cloud-storage/src/main/java/org/jclouds/googlecloudstorage/binders/DefaultObjectAccessControlsBinder.java
+++ google-cloud-storage/src/main/java/org/jclouds/googlecloudstorage/binders/DefaultObjectAccessControlsBinder.java
@@ -14,25 +14,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jclouds.googlecloudstorage.handlers;
+package org.jclouds.googlecloudstorage.binders;
 
 import java.util.Map;
 
 import javax.inject.Inject;
-import org.jclouds.googlecloudstorage.domain.BucketAccessControls;
+
+import org.jclouds.googlecloudstorage.domain.templates.DefaultObjectAccessControlsTemplate;
 import org.jclouds.http.HttpRequest;
 import org.jclouds.rest.MapBinder;
 import org.jclouds.rest.binders.BindToJsonPayload;
 
-public class BucketAccessControlsBinder implements MapBinder {
+public class DefaultObjectAccessControlsBinder implements MapBinder {
 
    @Inject
    private BindToJsonPayload jsonBinder;
 
    @Override
    public <R extends HttpRequest> R bindToRequest(R request, Map<String, Object> postParams) {
-      BucketAccessControls postBucket = (BucketAccessControls) postParams.get("bucketAccessControls");
-      return bindToRequest(request, postBucket);
+      DefaultObjectAccessControlsTemplate template = (DefaultObjectAccessControlsTemplate) postParams.get("template");
+      return bindToRequest(request, template);
    }
 
    @Override
